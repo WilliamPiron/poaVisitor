be597a7e35779bca3d9c08ecf86160fcc6aff22b
wilpiron <william.piron@laposte.net>
2016-11-07 16:38:51 +0100

    Ajout de l'implémentation de IVisitor via la classe RapportCommande.
    Les méthodes ne font rien pour le moment.

---
+++ src/RapportCommandes.java
@@ -0,0 +1,25 @@
+/**
+ * Created by wilpiron on 07/11/2016.
+ */
+public class RapportCommandes implements IVisitor {
+
+    @Override
+    public void visit(Client client) {
+
+    }
+
+    @Override
+    public void visit(Commande commande) {
+
+    }
+
+    @Override
+    public void visit(Ligne ligne) {
+
+    }
+
+    @Override
+    public void afficherCommande() {
+
+    }
+}
